4dd2de916cfbd58ae1ed4cf7636b2e71bfe67cd3
Maximilian Lackaw <mlackaw@gmail.com>
2016-06-18 16:07:15 +0200

    Add simple save book route

--- app.js
+++ app.js
@@ -1,21 +1,31 @@
 'use strict';
 
-const express  = require('express');
-const mongoose = require('mongoose');
-const Book = require('./models/bookModel');
+const express    = require('express');
+const mongoose   = require('mongoose');
+const Book       = require('./models/bookModel');
+const bodyParser = require('body-parser');
 
 const db = mongoose.connect('mongodb://mint-vm:27017/bookAPI');
 
 const app = express();
 const port = process.env.PORT || 3000;
 
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
 const bookRouter = express.Router();
 
 bookRouter.route('/Books')
+  .post((req, res) => {
+    const book = new Book(req.body);
+
+    book.save();
+    res.status(201).send(book);
+  })
   .get((req, res) => {
     let query = {};
 
-    if (req.query.genre){
+    if (req.query.genre) {
       query.genre = req.query.genre;
     }
 
